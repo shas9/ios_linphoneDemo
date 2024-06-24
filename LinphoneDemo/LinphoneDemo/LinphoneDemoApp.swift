97ff530bc5f89d1705d7f659eb18c94bb885cad4
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2024-06-24 23:52:24 +0600

    Initialize application

---
+++ LinphoneDemo/LinphoneDemo/LinphoneDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinphoneDemoApp.swift
+//  LinphoneDemo
+//
+//  Created by Shahwat Hasnaine on 24/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LinphoneDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
